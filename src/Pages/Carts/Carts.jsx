9a5aa21012175de47ecb8eaadd2a466563029280
TranToan299 <toantran58.k44@gmail.com>
2022-12-17 22:21:44 +0700

    cart và profile UI

--- src/Pages/Carts/Carts.jsx
+++ src/Pages/Carts/Carts.jsx
@@ -1,7 +1,86 @@
+
 import React from "react";
 
-const CArts = () => {
-  return <div>CArts</div>;
+const Carts = () => {
+  return <>
+    <div className="container-fluid">
+      <h3>Carts</h3>
+      <hr></hr>
+      <table className="table" >
+        <thead style={{ border: '1px solid transparent' }}>
+          <tr className="text-center">
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>
+              ID
+            </th>
+            <th>
+              Image
+            </th>
+            <th>
+              Name
+            </th>
+            <th>
+              Price
+            </th>
+            <th>
+              Quantity
+            </th>
+            <th>
+              Total
+            </th>
+            <th>
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody className="text-center" style={{ border: '1px solid transparent' }}>
+          <tr>
+            <td>
+              <input type="checkbox" checked={true}/>
+            </td>
+            <td>
+              1
+            </td>
+            <td>
+              <img src='' alt='...' />
+            </td>
+            <td>
+              Product 1
+            </td>
+            <td>
+              1000
+            </td>
+            <td>
+              <button className="btn btn-success">
+                <i class="fa fa-plus"></i>
+              </button>
+              <span className='cart-quantity'>1</span>
+              <button className="btn btn-danger">
+                <i class="fa fa-minus"></i>
+              </button>
+            </td>
+            <td>
+              1000
+            </td>
+            <td>
+              <button className="btn btn-success">EDIT</button>
+              <button className="btn btn-danger">DELETE</button>
+            </td>
+
+          </tr>
+        </tbody>
+        <tfoot style={{ border: '1px solid transparent' }}>
+          <tr>
+            <td colspan="8" className="text-end" >
+              <button className="btn btn-warning" >SUBMIT ORDER</button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  </>;
 };
 
-export default CArts;
+export default Carts;
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import Favourite from "./Favourite";
+import OrderHistory from "./OrderHistory";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [component, setComponent] = useState(true)
+  let renderProfileFooter = () => {
+    if (component) {
+      return <OrderHistory/>
+    }
+    return <Favourite />
+  }
+
+
+  return <>
+    <div className="container-fluid">
+      <div className="profile-container">
+        <h3>Profile</h3>
+        <div className="profile-content row">
+          <div className="col-2">
+            <div className="profile-image">
+              <img src="../../assets/image/image 3.png" alt="..." />
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="profile-left">
+              <div className="form-group">
+                <p>Email</p>
+                <input type="email" className="form-control" />
+              </div>
+              <div className="form-group">
+                <p>Phone</p>
+                <input type="phone" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="profile-right">
+              <div className="form-group">
+                <p>Name</p>
+                <input type="name" className="form-control" />
+              </div>
+              <div className="form-group">
+                <p>Password</p>
+                <input type="phone" className="form-control" />
+              </div>
+              <div class="gender row pt-5">
+                <div className="col-2">
+                  <p>Gender:</p>
+                </div>
+                <div class="male col-2 pt-md-3">
+                  <input
+                    type="radio"
+                    name="gender"
+                    id="male"
+                  />
+                  <label for='male'>Male</label>
+                </div>
+                <div class="female col-2 pt-md-3">
+                  <input
+                    type="radio"
+                    name="gender"
+                    id="female"
+                    checked={true}
+                  />
+                  <label for='female'>Female</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="profile-footer">
+        <button className="btn" onClick={() => {
+          setComponent(true)
+        }}>Order history</button>
+        <button className="btn" onClick={() => {
+          setComponent(false)
+        }}>Favourite</button>
+
+
+        {renderProfileFooter()}
+      </div>
+    </div>
+
+
+  </>;
 };
 
 export default Profile;
