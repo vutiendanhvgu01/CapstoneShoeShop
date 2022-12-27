560204ec325f5f8e33b724b6b732a68b41250fe9
Danh <vutiendanh.0101@gmail.com>
2022-12-27 11:12:10 +0700

    'responsive'

--- src/Pages/Profile/OrderHistory.jsx
+++ src/Pages/Profile/OrderHistory.jsx
@@ -1,49 +1,8 @@
 import React, { useState } from "react";
-import { Table } from "antd";
+
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { renderHistoryProduct } from "../../redux/reducers/productReducer";
-const columns = [
-  {
-    title: "ID",
-    dataIndex: "id",
-  },
-  {
-    title: "Image",
-    dataIndex: "image",
-    render: (image) => <img src={image} alt="..." />,
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-    sortOrder: "acsend",
-  },
-  {
-    title: "Quantity",
-    dataIndex: "quantity",
-  },
-  {
-    title: "Total",
-    dataIndex: "total",
-  },
-];
-const data = [
-  {
-    id: 1,
-    image:
-      "https://images.theconversation.com/files/430483/original/file-20211105-9897-18ahqx2.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
-  },
-  {
-    id: 2,
-    image:
-      "https://images.theconversation.com/files/430483/original/file-20211105-9897-18ahqx2.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
-  },
-  {
-    id: 3,
-    image:
-      "https://images.theconversation.com/files/430483/original/file-20211105-9897-18ahqx2.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
-  },
-];
 
 const OrderHistory = () => {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -4,94 +4,101 @@ import React, { useEffect, useRef, useState } from "react";
 import "../../assets/css/Search.css";
 import ShoeCard from "../../Components/ShoeCard/ShoeCard";
 import { ACCESS_TOKEN, getStore } from "../../util/config";
-import _ from 'lodash'
+import _ from "lodash";
 import { useSearchParams } from "react-router-dom";
 const Search = () => {
   let [searchParams, setSearchParams] = useSearchParams();
-  let keyword = useRef()
-  let [arrProduct, setArrProduct] = useState([])
+  let keyword = useRef();
+  let [arrProduct, setArrProduct] = useState([]);
   const handleChange = (e) => {
-    let value = e.target.value
+    let value = e.target.value;
     keyword.current = value;
-  }
+  };
   const callApiProduct = async () => {
-    let tuKhoa = searchParams.get('keyword')
+    let tuKhoa = searchParams.get("keyword");
     let result = await axios({
       url: `https://shop.cyberlearn.vn/api/Product?keyword=${tuKhoa}`,
       method: "GET",
-    })
-    let array = result.data.content
+    });
+    let array = result.data.content;
 
-    setArrProduct(array)
-  }
-  useEffect(()=>{
-    if (searchParams.get('keyword')) {
-      callApiProduct()
+    setArrProduct(array);
+  };
+  useEffect(() => {
+    if (searchParams.get("keyword")) {
+      callApiProduct();
     }
-
-  },[searchParams.get('keyword')])
+  }, [searchParams.get("keyword")]);
 
   const handleSearch = () => {
     setSearchParams({
-      keyword: keyword.current
-    })
-  }
+      keyword: keyword.current,
+    });
+  };
   const handleSort = (e) => {
-    let value = e.target.value
-    console.log(value)
-    let resultSort =_.orderBy(arrProduct,['price'],[value])
-    setArrProduct(resultSort)
-  }
+    let value = e.target.value;
+    console.log(value);
+    let resultSort = _.orderBy(arrProduct, ["price"], [value]);
+    setArrProduct(resultSort);
+  };
 
+  return (
+    <>
+      <div className="search">
+        <div className=" d-flex">
+          <div className="form-group">
+            <label for="keyword">Search</label>
 
-  return <>
-    <div className="container-fluid search">
-      <div className=" d-flex">
-        <div className="form-group">
-          <label for="keyword">
-            Search
-          </label>
-
-          <input type="text" className="form-control" id='keyword' style={{ width: '550px' }} onChange={handleChange} />
-
-        </div>
-        <button className="btn btn-search" onClick={() => {
-          handleSearch();
-        }}>Search</button>
-
-      </div>
-    </div>
-    <div className="search-content">
-      <h1>Search</h1>
-      <div className="container-fluid">
-        <div className="search-sort form-group ">
-          <p>Price</p>
-          <select id="sort" onChange={handleSort}>
-            <option value='sort' selected>Sort</option>
-            <option value="desc" name='desc'>Decrease</option>
-            <option value="asc" name='asc'>Ascending</option>
-          </select>
-        </div>
-        <div className="search-product">
-          <div className="row">
-
-            {arrProduct.map((item) => {
-              return <div className="col-4">
-                <ShoeCard prod={item} />
-              </div>
-            })}
-
+            <input
+              type="text"
+              className="form-control"
+              id="keyword"
+              style={{ width: "550px" }}
+              onChange={handleChange}
+            />
           </div>
-
+          <button
+            className="btn btn-search"
+            onClick={() => {
+              handleSearch();
+            }}
+          >
+            Search
+          </button>
         </div>
-
       </div>
-
-    </div>
-
-
-
-  </>;
+      <div className="search-content">
+        <h1>Search</h1>
+        <div>
+          <div className="search-sort form-group ">
+            <p>Price</p>
+            <select id="sort" onChange={handleSort}>
+              <option value="sort" selected>
+                Sort
+              </option>
+              <option value="desc" name="desc">
+                Decrease
+              </option>
+              <option value="asc" name="asc">
+                Ascending
+              </option>
+            </select>
+          </div>
+          <div className="search-product">
+            <div className="row">
+              {arrProduct.map((item) => {
+                return (
+                  <div className="col-lg-4 col-md-4 col-xl-4 col-xs-4">
+                    <ShoeCard prod={item} />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Search;
